dfeaa667228b39b2dcc3f440de61444517c2ab43
haruorange <44792672+haruorange@users.noreply.github.com>
2020-05-26 17:52:33 -0700

    player
    player can move

---
+++ PitchDemo/Assets/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float rotateSpeed = 4f;
+	[SerializeField] private float moveSpeed = 8f;
+	[SerializeField] private bool ableToPush = false;
+	private Rigidbody rb;
+	// Start is called before the first frame update
+	void Start()
+    {
+		rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Move();
+    }
+
+	void Move()
+	{
+		Vector3 rightMovement = Vector3.right * Input.GetAxis("HorizontalKey");
+		Vector3 upMovement = Vector3.forward * Input.GetAxis("VerticalKey");
+		Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+		if (Input.GetAxis("HorizontalKey") != 0 || Input.GetAxis("VerticalKey") != 0)
+		{
+			transform.forward = Vector3.RotateTowards(transform.forward, heading, rotateSpeed, 0f);
+		}
+		rb.velocity = heading * moveSpeed;
+	}
+
+	void push()
+	{
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Paint"))
+		{
+			ableToPush = true;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Paint"))
+		{
+			ableToPush = false;
+		}
+	}
+}
